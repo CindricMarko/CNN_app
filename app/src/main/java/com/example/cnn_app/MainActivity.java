f81eb572b9639cabdd147047919807de0b69e02e
cindricmarko <mcindric@etfos.hr>
2022-02-22 15:43:39 +0100

    dodana kamera

--- app/src/main/java/com/example/cnn_app/MainActivity.java
+++ app/src/main/java/com/example/cnn_app/MainActivity.java
@@ -2,11 +2,17 @@ package com.example.cnn_app;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
@@ -16,19 +22,24 @@ import android.widget.TextView;
 import com.example.cnn_app.ml.Model;
 
 import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.Tensor;
 import org.tensorflow.lite.support.image.TensorImage;
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    String currentImagePath = null;
+    private static final int IMAGE_REQUEST = 1;
     private ImageView imgView;
-    private Button select, predict;
+    private Button select, predict, camera;
     private TextView tv;
-    private Bitmap img;
+    private Bitmap img, captureImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +50,9 @@ public class MainActivity extends AppCompatActivity {
         tv = (TextView) findViewById(R.id.textView);
         select = (Button) findViewById(R.id.button);
         predict = (Button) findViewById(R.id.button2);
+        camera = (Button) findViewById(R.id.button3);
+
+
 
 
 
@@ -67,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
 
                     TensorImage tensorImage = new TensorImage(DataType.FLOAT32);
                     tensorImage.load(img);
+
                     ByteBuffer byteBuffer = tensorImage.getBuffer();
 
                     inputFeature0.loadBuffer(byteBuffer);
@@ -106,5 +121,36 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
+    }
+
+    public void CaptureImage(View view) {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(cameraIntent.resolveActivity(getPackageManager()) != null)
+        {
+             File imageFile = null;
+            try {
+                imageFile = getImageFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if(imageFile != null)
+            {
+                Uri imageUri = FileProvider.getUriForFile(this, "com.example.android.fileprovider", imageFile);
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(cameraIntent, IMAGE_REQUEST);
+            }
+        }
+
+    }
+
+    private File getImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageName = "jpg_"+timeStamp+"_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        File imageFile = File.createTempFile(imageName, ".jpg", storageDir);
+        currentImagePath = imageFile.getAbsolutePath();
+        return imageFile;
     }
 }
